c251504cfb7a1842ec52f4d37597cfa9827a4b54
Anidragon <aniakula92@gmail.com>
2023-05-18 22:33:39 -0400

    Update GameMain.py

--- GameMain.py
+++ GameMain.py
@@ -1,1 +1,16 @@
 import pygame
+Window = pygame.display.set_mode((1100, 500), pygame.RESIZABLE)
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                run = False
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
+
+
