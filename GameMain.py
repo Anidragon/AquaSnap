02f2ce57a557477bfd872a088caae76a9a53fa5e
Anidragon <aniakula92@gmail.com>
2023-05-19 09:52:21 -0400

    added background image, added background scaling, and other display items

--- GameMain.py
+++ GameMain.py
@@ -1,13 +1,30 @@
 import pygame
-Window = pygame.display.set_mode((1100, 500), pygame.RESIZABLE)
+# width and height
+Width = 1100
+Height = 500
+# setting display, caption, icon, and bg_img
+window = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("Aqua Snap")
 screenIcon = pygame.image.load('AquaSnapIcon.png')
+bg_img = pygame.image.load('BeachPixel.jpg')
+#scale of background
+scale = pygame.transform.scale(bg_img, (Width, Height))
 pygame.display.set_icon(screenIcon)
+
+
 def main():
+  
     run = True
     while run:
+        
+        window.blit(scale,(0,0))
         for event in pygame.event.get():
             if(event.type == pygame.QUIT):
                 run = False
+        
+
+        pygame.display.update()
+
     pygame.quit()
 
 if __name__ == "__main__":
